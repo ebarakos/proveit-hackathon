69dad98f7ef0000421da79e70e444cdb7e47d675
Evangelos Barakos <evangelos@zenolive.com>
2017-01-21 15:33:34 +0000

    pascal cases

--- contracts/CreditContract.sol
+++ contracts/CreditContract.sol
@@ -4,22 +4,22 @@ contract CreditContract {
 
 	mapping (address => uint) public credits;
 	mapping (address => mapping( uint => address )) public bills;
-    mapping (address => uint) public bills_count;
-    mapping (address => mapping(uint => address)) public outgoing_bills;
-    mapping (address => uint) public outgoing_bills_count;
+    mapping (address => uint) public billsCount;
+    mapping (address => mapping(uint => address)) public outgoingBills;
+    mapping (address => uint) public outgoingBillsCount;
     
     function CreditContract() {
 	}
 
     function AddBill(address _chargee, uint _amount, uint _end) returns (address bill){
-    	address new_bill = new Bill(_chargee, msg.sender, _amount, _end);
-    	bills[_chargee][bills_count[_chargee]] = new_bill;
-    	bills_count[_chargee] += 1;
+    	address newBill = new Bill(_chargee, msg.sender, _amount, _end);
+    	bills[_chargee][bills_count[_chargee]] = newBill;
+    	billsCount[_chargee] += 1;
 
-    	outgoing_bills[msg.sender][outgoing_bills_count[msg.sender]] = new_bill;
-    	outgoing_bills_count[msg.sender] += 1;
+    	outgoingBills[msg.sender][outgoingBillsCount[msg.sender]] = newBill;
+    	outgoingBills_count[msg.sender] += 1;
     	
-    	return new_bill;
+    	return newBill;
     }
 
     function AcceptBill(address chargee) returns(bool success){
@@ -63,6 +63,215 @@ contract Bill{
 		return true;
 	}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ............
+    
 	function Pay() payable returns(bool success){
 		if (msg.sender != chargee) return false;
 		if (msg.value < amount) return false;

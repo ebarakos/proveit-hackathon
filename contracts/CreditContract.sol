a9f917fb093dfa0efca5b1d4f91151f62d0d958c
Sigurd Lund <sigurd.lund@knowit.no>
2017-01-21 17:25:54 +0000

    Some changes in the calculation of credits

--- contracts/CreditContract.sol
+++ contracts/CreditContract.sol
@@ -1,40 +1,63 @@
-pragma solidity ^0.4.8;
+	pragma solidity ^0.4.8;
 
 contract CreditContract {
 
-	mapping (address => uint) public credits;
+	mapping (address => int) public credits;
 	mapping (address => mapping( uint => address )) public bills;
     mapping (address => uint) public billsCount;
     mapping (address => mapping(uint => address)) public outgoingBills;
     mapping (address => uint) public outgoingBillsCount;
+    int private range; 
     
     function CreditContract(){
 	}
 
     function AddBill(address _chargee, uint _amount, uint _end) returns (address bill){
     	address newBill = new Bill(_chargee, msg.sender, _amount, _end);
-    	bills[_chargee][bills_count[_chargee]] = newBill;
+    	bills[_chargee][billsCount[_chargee]] = newBill;
     	billsCount[_chargee] += 1;
 
     	outgoingBills[msg.sender][outgoingBillsCount[msg.sender]] = newBill;
-    	outgoingBills_count[msg.sender] += 1;
+    	outgoingBillsCount[msg.sender] += 1;
     	
     	return newBill;
     }
 
     function AcceptBill(address chargee) returns(bool success){
-    	credits[chargee] -= 5;
+    	credits[chargee] -= calculateCredits(chargee) / 2;
     	return true;
     }
 
-    function PayBill(address chargee){
-    	uint credit = 6;
-    	if (now > Bill(msg.sender).end()) credit = 4;
-    	credits[chargee] += credit;
+    function PayBill(address chargee) {
+    	if (now > Bill(msg.sender).end())
+    		credits[chargee] += calculateNegativeCredits(int(now - Bill(msg.sender).end()), credits[chargee]);
+    	else
+    		int creditOfReciever = credits[Bill(msg.sender).charger()];    	
+
+    		credits[chargee] += calculate(credits[chargee], creditOfReciever);
     }
+
+    function calculateNegativeCredits(int howLate, int prevValue) returns(int) {
+    	int numberOfDaysDelayed = howLate / 1 days;
+    	return prevValue - ((1 - 1 / (1 + numberOfDaysDelayed)) / 10);
+    }
+
+    function calculateCredits(address chargee) returns(int) {
+
+    	int chargeeCredit = credits[Bill(msg.sender).charger()];
+
+    	int prevCredit = credits[Bill(msg.sender).chargee()];
+
+    	return calculate(prevCredit, chargeeCredit);
+    }
+
+    function calculate(int prevValue, int creditOfOpponent) returns(int) {
+    	return range - range / (1 + creditOfOpponent);
+    }
+
 }
 
-contract Bill{
+contract Bill {
     
 	address public charger;
 	address public chargee;
@@ -63,220 +86,12 @@ contract Bill{
 		return true;
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    ............
-
 	function Pay() payable returns(bool success){
 		if (msg.sender != chargee) return false;
 		if (msg.value < amount) return false;
 		if (!charger.send(msg.value)) return false;
-		if(msg.value>amount) chargee.send(msg.value-amount);
+		if(msg.value < amount || !chargee.send(msg.value-amount)) 
+			return false;
 		paid = true;
 		creditContract.PayBill(msg.sender);
 		
